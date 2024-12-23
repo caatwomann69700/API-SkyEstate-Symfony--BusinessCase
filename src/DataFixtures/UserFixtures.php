bff5deaa0f0384e196c23f9d8b62a9b849176aa8
caatwomann69700 <sdebiani7@gmail.com>
2024-12-23 13:24:13 +0100

    💙

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'un utilisateur admin
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'adminpassword'));
+        $manager->persist($admin);
+
+        // Création de plusieurs utilisateurs standards
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+            $user->setEmail("user$i@example.com");
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'userpassword'));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
